38579d2e9ae18b57582023bfacf9c81625e827e2
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-09 18:05:48 +0100

    Create 1652115602423_create_table_properties.js

---
+++ migrations/1652115602423_create_table_properties.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "up": "CREATE TABLE properties (id INT AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, phone_number VARCHAR(255) NOT NULL, )",
+    "down": "DROP TABLE properties"
+}
