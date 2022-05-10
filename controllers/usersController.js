3a729baa01a944e86490c3fd876391edd2a17356
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-10 15:31:23 +0100

    User Logics

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -12,40 +12,44 @@ exports.findAll = (req, res) => {
 };
 
 exports.createUser = (req, res) => {
-        // Validate request
-        if (!req.body) {
-            res.status(400).send({
-                message: "Content can not be empty!"
-            });
-        }
+    // Validate request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
 
-        // Create a User
-        const { email, phone } = req.body;
-        const user = new User(email, phone);
+    // Create a User
+    const { email, phone } = req.body;
+    const user = new User(email, phone);
 
-        if (checkUser(user);
-
-            // Save User in the database
-            User.createUser(user, (err, data) => {
-                if (err)
-                    res.status(500).send({
-                        message: err.message || "Some error occurred while creating the User."
-                    });
-                else
-                    res.status(200).json({
-                        status: true,
-                        message: "User created successfully",
-                        data: data
-                    });
-            });
-        };
-
-        function checkUser(newUser, req, res) {
-            if (!User.checkUser(newUser)) {
-                console.log("Email Exist");
-                res.status(400).json({
-                    status: false,
-                    message: "Email or Password Exist"
+    if (checkUser(user) === false) {
+        console.log("Email Exist");
+        res.status(400).json({
+            status: false,
+            message: "Email or Phone Already Exist"
+        });
+    } else {
+        // Save User in the database
+        User.createUser(user, (err, data) => {
+            if (err)
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the User."
                 });
-            }
-        }
+            else
+                res.status(200).json({
+                    status: true,
+                    message: "User created successfully",
+                    data: data
+                });
+        });
+    }
+};
+
+function checkUser(newUser) {
+    if (User.checkUser(newUser) === false) {
+        return false;
+    } else {
+        return false;
+    }
+}
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+
 
 const app = express();
 
 
-app.use(cors());
+
 
 // parse requests of content-type - application/json
 app.use(express.json());
