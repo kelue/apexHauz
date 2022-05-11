adbdd02656ffd1c9c9ea2af920363948d9e28306
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-11 13:52:32 +0100

    Rearranged Folders

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/m", (req, res) => {
 });
 
 
-require("./routes/api/v1/usersRoutes.js")(app);
+require("./src/routes/api/v1/usersRoutes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.SERVER_PORT;
