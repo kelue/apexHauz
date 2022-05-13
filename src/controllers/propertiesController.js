60c99c01059c8ad95ec48a920bba2a8e981eeab6
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-13 12:14:50 +0100

    Properties

--- src/controllers/propertiesController.js
+++ src/controllers/propertiesController.js
@@ -16,3 +16,30 @@ exports.getAllProperties = (req, res) => {
         else res.send(data);
     });
 }
+
+// exports.getPropertiesById = (req, res) => {
+//     /* A function that returns a property by its id. */
+//     Properties.getById((err, data) => {
+//         if (err)
+//             res.status(500).send({
+//                 message: err.message || "Some error occurred while retrieving property."
+//             });
+//         else res.send(data);
+//     });
+// }
+
+exports.getPropertiesById = (req, res) => {
+    Properties.getById(Number(req.params.id), (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Properties with id ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Property with id " + req.params.id
+                });
+            }
+        } else res.send(data);
+    });
+};
