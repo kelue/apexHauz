07d5047f7aff40eb3ec5c89388ac34d89729e816
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-13 02:13:18 +0100

    Properties Functions

--- src/database/queries.js
+++ src/database/queries.js
@@ -17,16 +17,56 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`;
+const createTableCategories = `
+CREATE TABLE IF NOT EXISTS categories (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`;
+
+const createTableProperties = `
+CREATE TABLE IF NOT EXISTS properties (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    price INT NOT NULL,
+    description VARCHAR(255) DEFAULT NULL,
+    image VARCHAR(255) DEFAULT NULL,
+    is_sold BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)`;
 
 const createNewUser = `
-INSERT INTO users VALUES(null, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())`;
+INSERT INTO users VALUES(null, ? , ? , ? , ? , ? , ? , ? , NOW(), NOW())
+`;
 
-const findUserByEmail = `SELECT * FROM users WHERE email = ?`;
+const findUserByEmail = `
+SELECT * FROM users WHERE email = ? `;
+
+const createPropertiesUserForeignKeys = `
+ALTER TABLE properties ADD CONSTRAINT properties_user_id_users_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+`;
+
+const createPropertiesCategoriesForeignKeys = `
+ALTER TABLE properties ADD CONSTRAINT properties_categories_id_categories_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE;
+`;
+
+const getAllProperties = `
+SELECT * FROM properties
+`;
 
 module.exports = {
     createDB,
     dropDB,
     createTableUsers,
+    createTableCategories,
+    createTableProperties,
     createNewUser,
-    findUserByEmail
+    findUserByEmail,
+    createPropertiesUserForeignKeys,
+    createPropertiesCategoriesForeignKeys,
+    getAllProperties
 };
