e7b14fa27166174ad24461428ff5446ed601c077
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-14 15:55:44 +0100

    Update queries.js

--- src/database/queries.js
+++ src/database/queries.js
@@ -1,6 +1,8 @@
 const { DB_NAME } = require('../config/db.config');
 
+
 const createDB = `CREATE DATABASE IF NOT EXISTS ${DB_NAME}`;
+
 
 const dropDB = `DROP DATABASE IF EXISTS ${DB_NAME}`;
 
@@ -17,6 +19,8 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`;
+
+
 const createTableCategories = `
 CREATE TABLE IF NOT EXISTS categories (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -25,6 +29,7 @@ CREATE TABLE IF NOT EXISTS categories (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`;
+
 
 const createTableProperties = `
 CREATE TABLE IF NOT EXISTS properties (
@@ -41,9 +46,11 @@ CREATE TABLE IF NOT EXISTS properties (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )`;
 
+
 const createNewUser = `
 INSERT INTO users VALUES(null, ? , ? , ? , ? , ? , ? , ? , NOW(), NOW())
 `;
+
 
 const findUserByEmail = `
 SELECT * FROM users WHERE email = ? `;
@@ -53,13 +60,16 @@ const createPropertiesUserForeignKeys = `
 ALTER TABLE properties ADD CONSTRAINT properties_user_id_users_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 `;
 
+
 const createPropertiesCategoriesForeignKeys = `
 ALTER TABLE properties ADD CONSTRAINT properties_categories_id_categories_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE;
 `;
 
+
 const getAllProperties = `
 SELECT * FROM properties
 `;
+
 
 const getPropertyById = `
 SELECT * FROM properties WHERE id = ?
