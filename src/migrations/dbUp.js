cfa83c5e9b3e074d332c765fba347166b363206a
Ahmad Opeyemi Busari <ahmadinjo@users.noreply.github.com>
2022-05-16 11:49:34 +0100

    Fixed all bugs associated with migrations

--- src/migrations/dbUp.js
+++ src/migrations/dbUp.js
@@ -1,13 +1,11 @@
-const { createDB: createDBQuery } = require('../database/queries/tables_queries');
 const connection = require('../config/db.config.init');
+const { createDB: createDBQuery } = require('../database/queries/tables_queries');
 
-(() => {
-    connection.query(createDBQuery, (err, _) => {
-        if (err) {
-            console.log("error: ", err);
-            return;
-        }
-        console.log('DB Created');
-        process.exit(0);
-    });
-})();
+connection.query(createDBQuery, (err, _) => {
+    if (err) {
+        console.log("error: ", err);
+        return;
+    }
+    console.log('DB Created');
+    process.exit(0);
+});
