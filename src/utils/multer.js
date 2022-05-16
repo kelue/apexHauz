f85637f7b917b60532576a7e88bf942586d0d600
treasure567 <52765128+treasure567@users.noreply.github.com>
2022-05-16 21:43:55 +0100

    Update File Upload

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -22,11 +22,13 @@ const upload = multer({
     },
     fileFilter(req, file, cb) {
         // checks if the uploaded file is a valid image
-        if (!file.mimetype.match(/(jpg|jpeg|png)$/)) {
+        if (!file ?.mimetype.match(/(jpg|jpeg|png)$/)) {
             return cb(new Error('Please upload a valid image!'), false);
         }
         cb(null, true);
     },
 });
 
-module.exports = upload;
+module.exports = {
+    upload
+}
